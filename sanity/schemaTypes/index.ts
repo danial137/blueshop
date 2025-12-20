33034ded90c7f548f77fbe4a7af15acca4b5aa2b
danial137 <danial79fakhrabadi@gmail.com>
2025-12-20 16:03:54 +0330

    add sanity type

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -4,7 +4,8 @@ import {blockContentType} from './blockContentType'
 import {categoryType} from './categoryType'
 import {postType} from './postType'
 import {authorType} from './authorType'
+import { salesType } from './saleType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType],
+  types: [blockContentType, categoryType, postType, authorType,salesType],
 }
