60a81ea23e67c45a77f4681e381e990eeac3dac1
danial137 <danial79fakhrabadi@gmail.com>
2025-12-20 19:19:26 +0330

    add banner

--- components/DiscountBanner.tsx
+++ components/DiscountBanner.tsx
@@ -1,27 +1,38 @@
 import { SALE_QUERY_RESULT } from '@/sanity.types'
 import React from 'react'
-import { Carousel, CarouselContent, CarouselItem } from './ui/carousel'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel'
 import { Card, CardContent, CardHeader } from './ui/card'
+import { Badge } from './ui/badge'
+import Image from 'next/image'
+import { urlFor } from '@/sanity/lib/image'
 
 const DiscountBanner = ({ sales }: { sales: SALE_QUERY_RESULT }) => {
   return (
     <Carousel className='w-full max-h-screen-xl max-auto my-10'>
       <CarouselContent>
         {sales?.map((sale) => (
-          <CarouselItem key={sale?._rev}>
+          <CarouselItem key={sale?._id}>
             <Card>
-              <CardContent>
-                <div>
-                  <div>
-                    <p>{sale?.title}</p>
+              <CardContent className='p-0'>
+                <div className='flex flex-col md:flex-row items-center '>
+                  <div className='flex-1 p-6 md:px-12'>
+                    <Badge variant='secondary' className='mb-2 md:mb-4'>
+                      {sale?.badge} {sale?.discountAmount} %
+                      Off
+                    </Badge>
+                    <h2 className='text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight mb-2 md:mb-4'>{sale?.title}</h2>
                   </div>
+                  {sale?.image && <div>
+                    <Image src={urlFor(sale?.image).url()} alt="bannerImage" width={500} height={300} />
+                  </div>}
                 </div>
-
               </CardContent>
             </Card>
           </CarouselItem>
         ))}
       </CarouselContent>
+      <CarouselPrevious className='absolute left-2' />
+      <CarouselNext className='absolute right-2' />
     </Carousel>
   )
 }
