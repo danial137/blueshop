2b7b44e15238152424d29b6cf6592c96778184fd
danial137 <danial79fakhrabadi@gmail.com>
2025-12-20 18:29:11 +0330

    add discount

--- components/DiscountBanner.tsx
+++ components/DiscountBanner.tsx
@@ -1,9 +1,28 @@
 import { SALE_QUERY_RESULT } from '@/sanity.types'
 import React from 'react'
+import { Carousel, CarouselContent, CarouselItem } from './ui/carousel'
+import { Card, CardContent, CardHeader } from './ui/card'
 
-const DiscountBanner = ({sales}:{sales:SALE_QUERY_RESULT}) => {
+const DiscountBanner = ({ sales }: { sales: SALE_QUERY_RESULT }) => {
   return (
-    <div>DiscountBanner</div>
+    <Carousel className='w-full max-h-screen-xl max-auto my-10'>
+      <CarouselContent>
+        {sales?.map((sale) => (
+          <CarouselItem key={sale?._rev}>
+            <Card>
+              <CardContent>
+                <div>
+                  <div>
+                    <p>{sale?.title}</p>
+                  </div>
+                </div>
+
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
   )
 }
 
