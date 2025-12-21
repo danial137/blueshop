99540b7867bbe99c71b66e5a9cfc3905aa1bef33
danial137 <danial79fakhrabadi@gmail.com>
2025-12-21 11:57:50 +0330

    customize banner

--- components/DiscountBanner.tsx
+++ components/DiscountBanner.tsx
@@ -1,31 +1,60 @@
-import { SALE_QUERY_RESULT } from '@/sanity.types'
-import React from 'react'
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel'
-import { Card, CardContent, CardHeader } from './ui/card'
-import { Badge } from './ui/badge'
-import Image from 'next/image'
-import { urlFor } from '@/sanity/lib/image'
+import { SALE_QUERY_RESULT} from "@/sanity.types";
+import { urlFor } from "@/sanity/lib/image";
+import React from "react";
+import { Card, CardContent } from "./ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
 
-const DiscountBanner = ({ sales }: { sales: SALE_QUERY_RESULT }) => {
+const DiscountBanner = async ({ sales }: { sales: SALE_QUERY_RESULT }) => {
   return (
     <Carousel className="w-full max-w-(--breakpoint-xl) mx-auto mt-10 mb-5">
       <CarouselContent>
-        {sales?.map((sale) => (
+        {sales.map((sale) => (
           <CarouselItem key={sale?._id}>
-            <Card>
-              <CardContent className='p-0'>
-                <div className='flex flex-col md:flex-row items-center '>
-                  <div className='flex-1 p-6 md:px-12'>
-                    <Badge variant='secondary' className='mb-2 md:mb-4'>
-                      {sale?.badge} {sale?.discountAmount} %
-                      Off
+            <Card className="overflow-hidden">
+              <CardContent className="p-0">
+                <div className="flex flex-col md:flex-row items-center">
+                  <div className="flex-1 p-6 md:px-12">
+                    <Badge
+                      variant="secondary"
+                      className="mb-2 md:mb-4 text-darkBlue capitalize"
+                    >
+                      {sale?.badge} {sale?.discountAmount}% off
                     </Badge>
-                    <h2 className='text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight mb-2 md:mb-4'>{sale?.title}</h2>
-                    <p className='text-muted-foreground'>{sale?.description}</p>
+                    <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight mb-2 md:mb-4">
+                      {sale.title}
+                    </h2>
+                    <p className="text-muted-foreground mb-4">
+                      {sale?.description}
+                    </p>
+                    <p className="mb-4">
+                      Use code:{" "}
+                      <span className="font-semibold text-primary uppercase">
+                        {sale.couponCode}
+                      </span>{" "}
+                      for{" "}
+                      <span className="font-semibold">
+                        {sale?.discountAmount}%
+                      </span>{" "}
+                      OFF
+                    </p>
+                    <Button>Shop Now</Button>
                   </div>
+
                   {sale?.image && (
-                    <div className='w-full md:w-1/2 h-auto flex itesm-center justify-center py-2'>
-                      <Image src={urlFor(sale?.image).url()} alt="bannerImage" width={500} height={500} objectFit='cover' className='h-full transition-transform hover:scale-105 ease-in-out ' />
+                    <div className="w-full md:w-1/2 h-auto relative flex items-center justify-center py-2">
+                      <img
+                        src={urlFor(sale?.image).size(1000, 1000).url()}
+                        alt={"bannerImage"}
+                        className="h-full object-cover transition-transform duration-300 ease-in-out hover:scale-105"
+                      />
                     </div>
                   )}
                 </div>
@@ -34,10 +63,10 @@ const DiscountBanner = ({ sales }: { sales: SALE_QUERY_RESULT }) => {
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className='absolute left-2' />
-      <CarouselNext className='absolute right-2' />
+      <CarouselPrevious className="absolute left-2" />
+      <CarouselNext className="absolute right-2" />
     </Carousel>
-  )
-}
+  );
+};
 
-export default DiscountBanner
+export default DiscountBanner;
