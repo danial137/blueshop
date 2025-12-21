3ee38b29e4045e09cfdfc792569c5c47e84746f1
danial137 <danial79fakhrabadi@gmail.com>
2025-12-21 11:36:36 +0330

    add banner

--- components/DiscountBanner.tsx
+++ components/DiscountBanner.tsx
@@ -8,7 +8,7 @@ import { urlFor } from '@/sanity/lib/image'
 
 const DiscountBanner = ({ sales }: { sales: SALE_QUERY_RESULT }) => {
   return (
-    <Carousel className='w-full max-h-screen-xl max-auto my-10'>
+    <Carousel className="w-full max-w-(--breakpoint-xl) mx-auto mt-10 mb-5">
       <CarouselContent>
         {sales?.map((sale) => (
           <CarouselItem key={sale?._id}>
@@ -21,10 +21,13 @@ const DiscountBanner = ({ sales }: { sales: SALE_QUERY_RESULT }) => {
                       Off
                     </Badge>
                     <h2 className='text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight mb-2 md:mb-4'>{sale?.title}</h2>
+                    <p className='text-muted-foreground'>{sale?.description}</p>
                   </div>
-                  {sale?.image && <div>
-                    <Image src={urlFor(sale?.image).url()} alt="bannerImage" width={500} height={300} />
-                  </div>}
+                  {sale?.image && (
+                    <div className='w-full md:w-1/2 h-auto flex itesm-center justify-center py-2'>
+                      <Image src={urlFor(sale?.image).url()} alt="bannerImage" width={500} height={500} objectFit='cover' className='h-full transition-transform hover:scale-105 ease-in-out ' />
+                    </div>
+                  )}
                 </div>
               </CardContent>
             </Card>
