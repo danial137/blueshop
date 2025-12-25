797f4a0ca27ffa423c1fbeeeacbcf427cd18a6cf
danial137 <danial79fakhrabadi@gmail.com>
2025-12-25 14:59:51 +0330

    add handleremoveproduct

--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -22,11 +22,11 @@ const AddToCartButton = ({ product, className }: Props) => {
                 <div className="text-sm">
                     <div className="flex items-center justify-between">
                         <span className="text-xs text-muted-foreground">Quantity</span>
-                        <QuantityButton />
+                        <QuantityButton  product={product}/>
                     </div>
-                    <div>
+                    <div className="flex items-center justify-between border-t pt-1">
                         <span>Subtotal</span>
-                        <PriceFormatter amount={product?.price ? product?.price * itemCount:0}/>
+                        <PriceFormatter amount={product?.price ? product?.price * itemCount : 0} />
                     </div>
                 </div>
             ) : (<Button onClick={handleAddToCart} className={cn('bg-dark-blue/10 text-black border-darkBlue border py-2 mt-2 w-full rounded-md font-medium hover:bg-darkBlue hover:text-white hoverEffect disabled:hover:cursor-not-allowed disabled:bg-darkBlue/10 disabled:text-gray-400 disabled:hover:text-gray-400 disabled:border-darkBlue/10', className)}>Add to Cart</Button>)}
--- components/QuantityButton.tsx
+++ components/QuantityButton.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Product } from "@/sanity.types"
+import { Button } from "./ui/button";
+import { HiMinus, HiPlus } from "react-icons/hi2"
+import toast from "react-hot-toast";
 
-const QuantityButton = () => {
+interface Props {
+    product: Product;
+    className?: string
+    borderStyle?: string
+}
+
+const QuantityButton = ({ product, className, borderStyle }: Props) => {
+    const handleRemoveProduct = () => {
+        toast.success('remove product successfully')
+    }
     return (
-        <div>QuantityButton</div>
+        <div>
+            <Button variant={'outline'} size='icon' className="w-6 h-6" onClick={handleRemoveProduct}><HiPlus /></Button>
+        </div>
     )
 }
 
