c7df3113737442ff30508b317136221d23de740b
danial137 <danial79fakhrabadi@gmail.com>
2026-02-11 17:27:25 +0330

    add category find section

--- components/CategorySelector.tsx
+++ components/CategorySelector.tsx
@@ -2,7 +2,10 @@
 import { Category } from '@/sanity.types'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
-import { Popover } from './ui/popover'
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+import { Button } from './ui/button'
+import { ChevronsUpDown } from 'lucide-react'
+import { Command, CommandInput } from './ui/command'
 interface Props {
     categories: Category[]
 }
@@ -12,8 +15,24 @@ const CategorySelector = ({ categories }: Props) => {
     const [value, setValue] = useState("");
     const router = useRouter()
 
-    return <Popover>
-        
+    return <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger>
+            <Button variant='outline' role='combobox' aria-expanded={open} className="w-[200px] justify-between">
+                {value ? categories.find((category) => category?._id === value)?.title : 'Filter by Category'}
+                <ChevronsUpDown />
+            </Button>
+        </PopoverTrigger>
+        <PopoverContent className='w-[200px] p-0'>
+            <Command>
+                <CommandInput placeholder='Search category...' className='h-9' onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        const selectedCategory = categories.find((c) => c.title?.toLowerCase().includes(e.currentTarget.value.toLocaleLowerCase())
+                        );
+                    }
+                }}
+                />
+            </Command>
+        </PopoverContent>
     </Popover>
 
 
